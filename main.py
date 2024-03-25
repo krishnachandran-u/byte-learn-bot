83cf70a440469b6cd691672514a33318b0f8b6df
krishnachandran-u <kriss52450@gmail.com>
2024-03-25 12:52:33 +0530

    deployment success check

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ BOT_USERNAME: Final = "@byte_learn_bot"
 async def startCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello! I'm a bot that helps you learn.")
 
+def handle_response(text: str) -> str:
+    if(text == 'hi'):
+        return 'Hello!'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text: str = update.message.text
+    response: str = handle_response(text)
+    await update.message.reply_text(response)
+
 if __name__ == "__main__":
     print('starting bot')
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", startCommand))
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(MessageHandler(filters.Text, handle_message))
 
     app.add_error_handler(error)
 
