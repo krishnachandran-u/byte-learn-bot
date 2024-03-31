34a04c1cd43d499696ffcad1300e611a739ae0c1
Krishnachandran U <kriss52450@gmail.com>
2024-03-31 13:07:33 +0530

    localize user_id to async functions

--- main.py
+++ main.py
@@ -21,16 +21,11 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 TOKEN = getenv('TOKEN')
-USERID = 0
 
 dp = Dispatcher()
 
-slots_ref = db.collection('users').document(str(USERID)).collection('slots')
-
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    global USERID
-    USERID = message.from_user.id
     await message.answer(
 """Welcome! I'm ByteLearn Bot, a learning bot that delivers daily knowledge bites.
 Want to learn something new? Just tell me a topic, and I'll send you interesting articles, videos, or other resources to get you started.
@@ -40,15 +35,13 @@ Ready to explore? Let's go!
 /viewslot slot_name - View resources in a slot
 /newslot - Create a new slot
 /editslot - Edit a slot
-/deleteslot - Delete a slot
+/deleteslot slot_name - Delete a slot
 """)
         
 @dp.message(Command('slots'))
 async def command_slots_handler(message: Message) -> None:
-    global USERID
-    global slots_ref
-
-    slots_ref = db.collection('users').document(str(USERID)).collection('slots')
+    user_id = message.from_user.id
+    slots_ref = db.collection('users').document(str(user_id)).collection('slots')
     slots = slots_ref.stream()
     reply = ""
     for doc in slots:
@@ -58,8 +51,8 @@ async def command_slots_handler(message: Message) -> None:
 
 @dp.message(Command('viewslot'))
 async def command_viewslot_handler(message: Message) -> None:
-    global USERID
-    global slots_ref
+    user_id = message.from_user.id
+    slots_ref = db.collection('users').document(str(user_id)).collection('slots')
 
     slot_name = message.text.split(' ')[1]
 
@@ -76,8 +69,8 @@ async def command_viewslot_handler(message: Message) -> None:
 
 @dp.message(Command('deleteslot'))
 async def command_deleteslot_handler(message: Message) -> None:
-    global USERID
-    global slots_ref
+    user_id = message.from_user.id
+    slots_ref = db.collection('users').document(str(user_id)).collection('slots')
 
     slot_name = message.text.split(' ')[1]
 
